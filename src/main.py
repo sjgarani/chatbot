22ecbf6e803c520ba26a9bb90a8ba61af5e48b24
sandro.garani <sandro.garani@gmail.com>
2025-04-11 22:12:26 +0000

    feat: add GraphCypherQAChain in chat response

--- src/main.py
+++ src/main.py
@@ -1,13 +1,23 @@
 import os
 from flask import Flask, send_file, request, jsonify
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_neo4j import Neo4jGraph
+from langchain_neo4j import GraphCypherQAChain
 
 app = Flask(__name__)
 
 conversation_history = []
 
+load_dotenv()
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+enhanced_graph = Neo4jGraph(enhanced_schema=True)
+chain = GraphCypherQAChain.from_llm(graph=enhanced_graph, llm=llm, verbose=True, allow_dangerous_requests=True)
+
 def get_bot_response(user_message):
-    # Basic static response for now.  Replace with actual chatbot logic.
-    return f"You said: {user_message}" 
+    answer = chain.invoke({"query": "{user_message}"})
+    print(answer)
+    return answer['result']
 
 @app.route("/")
 def index():
